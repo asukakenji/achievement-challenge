414a8b6605b75aeb2db86ecb43764ff7572d0a32
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-11 00:29:19 +0800

    Blocked the submit button when previous request is in progress; Added dialog box responses

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,14 @@
 'use strict';
 
+// TODO: Handle "enter" typed at "password" field
+// TODO: Handle tab index
+
 $(document).ready(function () {
   function hideDisplayNoneByDefault() {
     $('.display_none_by_default').addClass('display_none');
   }
-  hideDisplayNoneByDefault();
 
-  $('#submit').click(function () {
+  function onSubmitClicked() {
     hideDisplayNoneByDefault();
     var username = $('#username').val();
     var password = $('#password').val();
@@ -15,6 +17,7 @@ $(document).ready(function () {
     } else if (password === undefined || password === null || password === '') {
       $('#password_empty').removeClass('display_none');
     } else {
+      $('#submit').off('click', onSubmitClicked);
       $.ajax({
         method: 'POST',
         dataType: 'json',
@@ -29,6 +32,28 @@ $(document).ready(function () {
             'textStatus': textStatus,
             'jqXHR': jqXHR
           });
+          switch (data.errorCode) {
+            case -2: {
+              alert('Error occurred when creating user!');
+              break;
+            }
+            case -1: {
+              alert('Incorrect password!');
+              break;
+            }
+            case 0: {
+              alert('Login successful');
+              break;
+            }
+            case 1: {
+              alert('New user created!');
+              break;
+            }
+            default: {
+              alert('Unknown error:' + data.errorCode);
+            }
+          }
+          $('#submit').on('click', onSubmitClicked);
         },
         error: function (jqXHR, textStatus, errorThrown) {
           console.log({
@@ -36,8 +61,12 @@ $(document).ready(function () {
             'textStatus': textStatus,
             'errorThrown': errorThrown
           });
+          $('#submit').on('click', onSubmitClicked);
         }
       });
     }
-  });
+  }
+
+  hideDisplayNoneByDefault();
+  $('#submit').on('click', onSubmitClicked);
 });
