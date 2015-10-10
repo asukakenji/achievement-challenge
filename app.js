d6c7140c6a4add7eb751752b0d2de425aff0d650
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-10 17:10:22 +0800

    Added body-parser as dependency

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 let express = require('express');
 let path = require('path');
 
+//let bodyParser = require('body-parser');    // Moved to route-specific
+
 let routes = require('./routes/index');
 
 let app = express();
@@ -11,9 +13,12 @@ let app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// Middlewares
+//app.use(bodyParser.urlencoded({ extended: false }));    // Moved to route-specific
+app.use('/', express.static(path.join(__dirname, 'public')));
+
 // Contents
 app.use('/', routes);
-app.use('/', express.static(path.join(__dirname, 'public')));
 
 // 404
 app.use(function(req, res, next) {
@@ -24,7 +29,7 @@ app.use(function(req, res, next) {
 
 // 404 || 500
 if (app.get('env') === 'development') {
-app.use(function(err, req, res, next) {
+  app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,32 @@
 let express = require('express');
 let router = express.Router();
 
+let bodyParser = require('body-parser');
+let urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+/* GET home page. */
 router.get('/', function (req, res, next) {
   res.redirect(302, '/main.html');
+});
+
+/* POST login page */
+router.post('/login', urlencodedParser, function (req, res, next) {
+  if (!req.body) return res.sendStatus(400)
+  let username = req.body.username;
+  let password = req.body.password;
+  if (username === 'kenji' && password === '123') {
+    let message = {
+      "errorCode": 0,
+      "url": "/game.html",
+      "token": "1234567890ABCDEF"
+    };
+    res.send(message);
+  } else {
+    let message = {
+      "errorCode": 1
+    };
+    res.send(message);
+  }
 });
 
 module.exports = router;
