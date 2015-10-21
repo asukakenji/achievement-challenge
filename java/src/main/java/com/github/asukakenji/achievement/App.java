84aa0f4e658f11b43e0d7694143dff77b1be4776
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-21 21:04:56 +0800

    Added java/ which was generated by Maven

---
+++ java/src/main/java/com/github/asukakenji/achievement/App.java
@@ -0,0 +1,13 @@
+package com.github.asukakenji.achievement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
