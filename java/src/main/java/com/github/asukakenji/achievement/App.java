9b548e27588ac68c67b89fb3a391999e5f8b8f19
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-22 02:53:02 +0800

    Reformatted source code

--- java/src/main/java/com/github/asukakenji/achievement/App.java
+++ java/src/main/java/com/github/asukakenji/achievement/App.java
@@ -4,10 +4,8 @@ package com.github.asukakenji.achievement;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
