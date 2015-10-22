352a83279e6c50309e326b033cbe8d68daf64cec
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-22 21:45:17 +0800

    Updated the draft of TriggerTarget

--- java/src/main/java/com/github/asukakenji/achievement/TriggerTarget.java
+++ java/src/main/java/com/github/asukakenji/achievement/TriggerTarget.java
@@ -1,9 +1,27 @@
 package com.github.asukakenji.achievement;
 
+/**
+ * {@code TriggerTarget} is the interface for targets of a trigger. A trigger
+ * target contains properties that could be manipulated. A subset of MongoDB
+ * operators are supported.
+ *
+ * @author asukakenji
+ *
+ */
 public interface TriggerTarget {
 
 	Class<?> getPropertyType(String key);
 
 	Object getProperty(String key);
+
+	//<T> boolean inProperty(String key, T element);
+
+	<T> void setProperty(String key, T value);
+
+	<T> void incProperty(String key, T amount);
+
+	//<T> void pushProperty(String key, T element);
+
+	//<T> void addPropertyToSet(String key, T element);
 
 }
