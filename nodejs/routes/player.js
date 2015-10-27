4a60118f69c850743084c12f46d2138991447c8a
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-27 11:57:16 +0800

    Switched to a ECMAScript 6 class; Switched to proper CommonJS mobule;

--- nodejs/routes/player.js
+++ nodejs/routes/player.js
@@ -1,23 +1,26 @@
 'use strict';
 
 /* Constructor of Player */
-function Player(username, password) {
-  // User properties
-  this._id = username;
-  this.password = password;
+class Player {
+  constructor(username, password) {
+    // User properties
+    this._id = username;
+    this.password = password;
 
-  // Player properties
-  this.lv = 1;
-  this.xp = 0;
-  this.loyalty = 100;
-  this.chips = 3000;
-  this.achievements = [];
-  this.mbox = [];
+    // Player properties
+    this.lv = 1;             // Level
+    this.xp = 0;             // Experience Points
+    this.lp = 100;           // Loyalty Points
+    this.chips = 3000;       // Chips
+    this.achievements = [];  // Achievements
+    this.mails = [];         // Mails
 
-  // Player properties (accumulated)
-  this.wagerAccumulated = 0;
-  this.bonusAccumulated = 0;
-  this.spinAccumulated = 0;
+    // Player properties (accumulated)
+    this.spinAccumulated = 0;    // Spin = Number of Spins
+    this.wagerAccumulated = 0;   // Wager = Chips Paid for a Spin
+    this.payoutAccumulated = 0;  // Payout = Chips Won for a Spin
+    this.bonusAccumulated = 0;   // Bonus = Chips Got from Non-Spin Means
+  }
 }
 
-return Player;
+module.exports = Player;
