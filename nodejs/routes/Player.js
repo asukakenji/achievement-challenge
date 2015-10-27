208f1936d6b5ce1668c729219d0dc7d86bec11bd
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-27 19:59:16 +0800

    Updated Player.js: Added all parameter constructor and checkings

--- nodejs/routes/Player.js
+++ nodejs/routes/Player.js
@@ -1,26 +1,30 @@
 'use strict';
 
+let Utils = require('../lib/Utils');
+
 class Player {
+
   /* Constructor of Player */
-  constructor(username, password) {
+  constructor(username, password, lv, xp, lp, chips, achievements, mails, spinAccumulated, wagerAccumulated, payoutAccumulated, bonusAccumulated) {
     // User properties
-    this._id = username;
-    this.password = password;
+    this._id = Utils.checkNonEmptyString(username);
+    this.password = Utils.checkNonEmptyString(password);
 
     // Player properties
-    this.lv = 1;             // Level
-    this.xp = 0;             // Experience Points
-    this.lp = 100;           // Loyalty Points
-    this.chips = 3000;       // Chips
-    this.achievements = [];  // Achievements
-    this.mails = [];         // Mails
+    this.lv = Utils.checkNumber(lv, 1);                      // Level
+    this.xp = Utils.checkNumber(xp, 0);                      // Experience Points
+    this.lp = Utils.checkNumber(lp, 100);                    // Loyalty Points
+    this.chips = Utils.checkNumber(chips, 3000);             // Chips
+    this.achievements = Utils.checkArray(achievements, []);  // Achievements
+    this.mails = Utils.checkArray(mails, []);                // Mails
 
     // Player properties (accumulated)
-    this.spinAccumulated = 0;    // Spin = Number of Spins
-    this.wagerAccumulated = 0;   // Wager = Chips Paid for a Spin
-    this.payoutAccumulated = 0;  // Payout = Chips Won for a Spin
-    this.bonusAccumulated = 0;   // Bonus = Chips Got from Non-Spin Means
+    this.spinAccumulated = Utils.checkNumber(spinAccumulated, 0);      // Spin = Number of Spins
+    this.wagerAccumulated = Utils.checkNumber(wagerAccumulated, 0);    // Wager = Chips Paid for a Spin
+    this.payoutAccumulated = Utils.checkNumber(payoutAccumulated, 0);  // Payout = Chips Won for a Spin
+    this.bonusAccumulated = Utils.checkNumber(bonusAccumulated, 0);    // Bonus = Chips Got from Non-Spin Means
   }
+
 }
 
 module.exports = Player;
