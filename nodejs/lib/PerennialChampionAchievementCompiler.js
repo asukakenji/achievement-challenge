2c0f24e0758ee93321fa47d91aea3822736a2bf3
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-28 19:47:47 +0800

    Added PerennialChampionAchievementCompiler.js

---
+++ nodejs/lib/PerennialChampionAchievementCompiler.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let Utils = require('./Utils');
+
+/**
+ * Perennial Champion - Achievement for payout amount (total chips won)
+ */
+class PerennialChampionAchievementCompiler {
+
+  /**
+   * Compiles a perennial champion achievement object to a trigger object.
+   */
+  static compile(pc_object) {
+    let rank = Utils.checkString(pc_object.rank);
+    let payoutAccumulated = Utils.checkNumber(pc_object.payoutAccumulated);
+    let xp = Utils.checkNumber(pc_object.xp);
+    if (payoutAccumulated < 0) throw new RangeError('Invalid payoutAccumulated value');
+    if (xp < 0) throw new RangeError('Invalid xp value');
+    let name = 'pc_' + rank;
+    let trigger_object = {
+      'name': name,
+      'queue': 'pc',
+      'target': 'player',
+      'condition': {
+        'payoutAccumulated': { '$gte': payoutAccumulated }
+      },
+      'action': {
+        '$push': {
+          'mails': {
+            'name': name,
+            'xp': xp
+          }
+        }
+      }
+    };
+    //TODO: trigger_object.__proto__ = Trigger.prototype;
+    return trigger_object;
+  }
+
+}
+
+module.exports = PerennialChampionAchievementCompiler;
