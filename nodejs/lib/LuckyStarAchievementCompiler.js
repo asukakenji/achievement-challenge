3c553f406752416fe4d0673e7aea9b52cb7765df
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-28 19:47:26 +0800

    Added LuckyStarAchievementCompiler.js

---
+++ nodejs/lib/LuckyStarAchievementCompiler.js
@@ -0,0 +1,42 @@
+'use strict';
+
+let Utils = require('./Utils');
+
+/**
+ * Lucky Star - Achievement for payout amount (chips won in one spin)
+ */
+class LuckyStarAchievementCompiler {
+
+  /**
+   * Compiles a lucky star achievement object to a trigger object.
+   */
+  static compile(ls_object) {
+    let rank = Utils.checkString(ls_object.rank);
+    let payout = Utils.checkNumber(ls_object.payout);
+    let lp = Utils.checkNumber(ls_object.lp);
+    if (payout < 0) throw new RangeError('Invalid payout value');
+    if (lp < 0) throw new RangeError('Invalid lp value');
+    let name = 'ls_' + rank;
+    let trigger_object = {
+      'name': name,
+      'queue': 'ls',
+      'target': 'spin',
+      'condition': {
+        'payout': { '$gte': payout }
+      },
+      'action': {
+        '$push': {
+          'mails': {
+            'name': name,
+            'lp': lp
+          }
+        }
+      }
+    };
+    //TODO: trigger_object.__proto__ = Trigger.prototype;
+    return trigger_object;
+  }
+
+}
+
+module.exports = LuckyStarAchievementCompiler;
