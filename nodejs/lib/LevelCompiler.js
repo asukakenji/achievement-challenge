cebc65312492710f231be43f12337f624de1513b
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-28 17:03:55 +0800

    Added LevelCompiler.js

---
+++ nodejs/lib/LevelCompiler.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let Utils = require('./Utils');
+
+class LevelCompiler {
+
+  /**
+   * Compiles a level object to a trigger object.
+   */
+  static compile(level_object) {
+    let lv = Utils.checkNumber(level_object.lv);
+    let xp = Utils.checkNumber(level_object.xp);
+    if (lv <= 0) throw new RangeError('Invalid lv value');
+    if (xp <= 0) throw new RangeError('Invalid xp value');
+    let name = 'lv' + Utils.padZero(3, lv + 1);
+    let trigger_object = {
+      'name': name,
+      'queue': 'lv',
+      'condition': {
+        'lv': { '$eq': lv },
+        'xp': { '$gte': xp }
+      },
+      'action': {
+        '$inc': { 'xp': -xp },
+        '$set': { 'lv': lv + 1 }
+      }
+    };
+    //TODO: trigger_object.__proto__ = Trigger.prototype;
+    return trigger_object;
+  }
+
+}
+
+module.exports = LevelCompiler;
