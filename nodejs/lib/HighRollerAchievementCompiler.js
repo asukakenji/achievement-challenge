0981c9d98b9117b043727c0edac21d2805455a3a
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-28 19:46:47 +0800

    Added HighRollerAchievementCompiler.js

---
+++ nodejs/lib/HighRollerAchievementCompiler.js
@@ -0,0 +1,45 @@
+'use strict';
+
+let Utils = require('./Utils');
+
+/**
+ * High Roller - Achievement for wager amount (chips spent in one spin)
+ */
+class HighRollerAchievementCompiler {
+
+  /**
+   * Compiles a high roller achievement object to a trigger object.
+   */
+  static compile(hr_object) {
+    let rank = Utils.checkString(hr_object.rank);
+    let wager = Utils.checkNumber(hr_object.wager);
+    let bonus = Utils.checkNumber(hr_object.bonus);
+    let xp = Utils.checkNumber(hr_object.xp);
+    if (wager < 0) throw new RangeError('Invalid wager value');
+    if (bonus < 0) throw new RangeError('Invalid bonus value');
+    if (xp < 0) throw new RangeError('Invalid xp value');
+    let name = 'hr_' + rank;
+    let trigger_object = {
+      'name': name,
+      'queue': 'hr',
+      'target': 'spin',
+      'condition': {
+        'wager': { '$gte': wager }
+      },
+      'action': {
+        '$push': {
+          'mails': {
+            'name': name,
+            'bonus': bonus,
+            'xp': xp
+          }
+        }
+      }
+    };
+    //TODO: trigger_object.__proto__ = Trigger.prototype;
+    return trigger_object;
+  }
+
+}
+
+module.exports = HighRollerAchievementCompiler;
