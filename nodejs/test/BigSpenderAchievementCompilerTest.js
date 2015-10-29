d78739760dc58d5826cdc4a3b3b0629a6d6d390d
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-29 10:08:18 +0800

    Added BigSpenderAchievementCompilerTest.js

---
+++ nodejs/test/BigSpenderAchievementCompilerTest.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let BigSpenderAchievementCompiler = require('../lib/BigSpenderAchievementCompiler');
+let assert = require('assert');
+
+describe('BigSpenderAchievementCompiler', function () {
+
+  describe('#compile()', function () {
+    it('should throw TypeError', function() {
+      let bs_object_1 = { 'rank': '', 'wagerAccumulated': 0, 'xp': 0 };
+      assert.throws(() => BigSpenderAchievementCompiler.compile(), TypeError);
+      assert.throws(() => BigSpenderAchievementCompiler.compile(null), TypeError);
+      assert.throws(() => BigSpenderAchievementCompiler.compile({}), TypeError);
+      assert.throws(() => BigSpenderAchievementCompiler.compile(bs_object_1), TypeError);
+    });
+    it('should throw RangeError', function() {
+      let bs_object_1 = { 'rank': 'XXX', 'wagerAccumulated': -1, 'xp': 0 };
+      let bs_object_2 = { 'rank': 'XXX', 'wagerAccumulated': 0, 'xp': -1 };
+      assert.throws(() => BigSpenderAchievementCompiler.compile(bs_object_1), RangeError);
+      assert.throws(() => BigSpenderAchievementCompiler.compile(bs_object_2), RangeError);
+    });
+    it('should compile the target correctly', function () {
+      assert.deepEqual(
+        BigSpenderAchievementCompiler.compile(
+          { 'rank': 'bronze', 'wagerAccumulated': 10000, 'xp': 1000 }
+        ),
+        {
+          'name': 'bs_bronze',
+          'queue': 'bs',
+          'target': 'player',
+          'condition': {
+            'wagerAccumulated': { '$gte': 10000 }
+          },
+          'action': {
+            '$push': {
+              'mails': {
+                'name': 'bs_bronze',
+                'xp': 1000
+              }
+            }
+          }
+        }
+      );
+    });
+  });
+
+});
