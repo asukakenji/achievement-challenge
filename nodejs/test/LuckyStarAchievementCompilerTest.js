a6f4c116f90f751818ecba631fc0a68db36e6c08
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-10-29 10:09:06 +0800

    Added LuckyStarAchievementCompilerTest.js

---
+++ nodejs/test/LuckyStarAchievementCompilerTest.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let LuckyStarAchievementCompiler = require('../lib/LuckyStarAchievementCompiler');
+let assert = require('assert');
+
+describe('LuckyStarAchievementCompiler', function () {
+
+  describe('#compile()', function () {
+    it('should throw TypeError', function() {
+      let ls_object_1 = { 'rank': '', 'payout': 0, 'lp': 0 };
+      assert.throws(() => LuckyStarAchievementCompiler.compile(), TypeError);
+      assert.throws(() => LuckyStarAchievementCompiler.compile(null), TypeError);
+      assert.throws(() => LuckyStarAchievementCompiler.compile({}), TypeError);
+      assert.throws(() => LuckyStarAchievementCompiler.compile(ls_object_1), TypeError);
+    });
+    it('should throw RangeError', function() {
+      let ls_object_1 = { 'rank': 'XXX', 'payout': -1, 'lp': 0 };
+      let ls_object_2 = { 'rank': 'XXX', 'payout': 0, 'lp': -1 };
+      assert.throws(() => LuckyStarAchievementCompiler.compile(ls_object_1), RangeError);
+      assert.throws(() => LuckyStarAchievementCompiler.compile(ls_object_2), RangeError);
+    });
+    it('should compile the target correctly', function () {
+      assert.deepEqual(
+        LuckyStarAchievementCompiler.compile(
+          { 'rank': 'bronze', 'payout': 10000, 'lp': 100 }
+        ),
+        {
+          'name': 'ls_bronze',
+          'queue': 'ls',
+          'target': 'spin',
+          'condition': {
+            'payout': { '$gte': 10000 }
+          },
+          'action': {
+            '$push': {
+              'mails': {
+               'name': 'ls_bronze',
+                'lp': 100
+              }
+            }
+          }
+        }
+      );
+    });
+  });
+
+});
