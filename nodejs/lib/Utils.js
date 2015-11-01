2fad33bf9c45ce265f4dc4c76375f1fd33eb9364
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-11-01 22:34:41 +0800

    Added checkNumber{GT,GTE,LT,LTE}(); Updated variable style; Updated "let" to "const"

--- nodejs/lib/Utils.js
+++ nodejs/lib/Utils.js
@@ -2,54 +2,80 @@
 
 class Utils {
 
-  static checkNonEmptyString(value, defaultValue) {
+  static checkNonEmptyString(value, default_value) {
     if (typeof value === 'string' && value !== '') {
-      if (defaultValue === undefined || typeof defaultValue === 'string') {
+      if (default_value === undefined || typeof default_value === 'string') {
         return value;
       }
-    } else if (typeof defaultValue === 'string' && defaultValue !== '') {
+    } else if (typeof default_value === 'string' && default_value !== '') {
       if (value === undefined || value === '') {
-        return defaultValue;
+        return default_value;
       }
     }
     throw new TypeError();
   }
 
-  static checkNumber(value, defaultValue) {
+  static checkNumber(value, default_value) {
     if (typeof value === 'number') {
-      if (defaultValue === undefined || typeof defaultValue === 'number') {
+      if (default_value === undefined || typeof default_value === 'number') {
         return value;
       }
-    } else if (typeof defaultValue === 'number') {
+    } else if (typeof default_value === 'number') {
       if (value === undefined) {
-        return defaultValue;
+        return default_value;
       }
     }
     throw new TypeError();
   }
 
-  static checkArray(value, defaultValue) {
+  static checkNumberGT(value, reference_value, default_value) {
+    const checked_value = this.checkNumber(value, default_value);
+    const checked_reference_value = this.checkNumber(reference_value);
+    if (checked_value > checked_reference_value) return checked_value;
+    throw new RangeError();
+  }
+
+  static checkNumberGTE(value, reference_value, default_value) {
+    const checked_value = this.checkNumber(value, default_value);
+    const checked_reference_value = this.checkNumber(reference_value);
+    if (checked_value >= checked_reference_value) return checked_value;
+    throw new RangeError();
+  }
+
+  static checkNumberLT(value, reference_value, default_value) {
+    const checked_value = this.checkNumber(value, default_value);
+    const checked_reference_value = this.checkNumber(reference_value);
+    if (checked_value < checked_reference_value) return checked_value;
+    throw new RangeError();
+  }
+
+  static checkNumberLTE(value, reference_value, default_value) {
+    const checked_value = this.checkNumber(value, default_value);
+    const checked_reference_value = this.checkNumber(reference_value);
+    if (checked_value <= checked_reference_value) return checked_value;
+    throw new RangeError();
+  }
+
+  static checkArray(value, default_value) {
     if (value instanceof Array) {
-      if (defaultValue === undefined || defaultValue instanceof Array) {
+      if (default_value === undefined || default_value instanceof Array) {
         return value;
       }
-    } else if (defaultValue instanceof Array) {
+    } else if (default_value instanceof Array) {
       if (value === undefined) {
-        return defaultValue;
+        return default_value;
       }
     }
     throw new TypeError();
   }
 
-  static padZero(totalDigitCount, number) {
-    totalDigitCount = this.checkNumber(totalDigitCount);
-    number = this.checkNumber(number);
-    if (totalDigitCount < 0) throw new RangeError('Invalid totalDigitCount value');
-    if (number < 0) throw new RangeError('Invalid number value');
-    let numberString = number.toString();
-    return (numberString.length < totalDigitCount)
-      ? ('0'.repeat(totalDigitCount - 1) + numberString).slice(-totalDigitCount)
-      : numberString;
+  static padZero(total_digit_count, number) {
+    const checked_total_digit_count = this.checkNumberGTE(total_digit_count, 0);
+    const checked_number = this.checkNumberGTE(number, 0);
+    const number_string = checked_number.toString();
+    return (number_string.length < checked_total_digit_count)
+      ? ('0'.repeat(checked_total_digit_count - 1) + number_string).slice(-checked_total_digit_count)
+      : number_string;
   }
 
 }
