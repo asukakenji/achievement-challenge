29aa433a19b7c89c4bb6f5e744b39973fab665e8
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2015-11-10 00:15:26 +0800

    Completed basic version of compile_trigger()

--- nodejs/lib/TriggerCompiler.js
+++ nodejs/lib/TriggerCompiler.js
@@ -145,7 +145,7 @@ function canonicalize_sub_field(name, comparison_operator, value) {
  * Optimizes a canonicalized condition object.
  */
 // private
-function optimized_condition(canonicalized_condition_object) {
+function optimize_condition(canonicalized_condition_object) {
   return canonicalized_condition_object;
 }
 
@@ -175,12 +175,189 @@ function canonicalize_action(action_object) {
 }
 
 /**
- * Compiles an optimized trigger object.
+ * Compiles an canonicalized trigger object.
  */
 // private
-function compile_trigger(optimized_trigger_object) {
-  return function() {
-    return false;
+function compile_trigger(canonicalized_trigger_object) {
+
+  function make_predicate(condition_object) {
+    const checked_condition_object = Utils.checkObject(condition_object);
+    const keys = Object.keys(checked_condition_object);
+    if (keys.length === 0) {
+      return function() {
+        return true;
+      };
+    } else if (keys.length === 1) {
+      const lop_or_name = keys[0];
+      if (lop_or_name.charAt(0) === '$') {
+        const value = checked_condition_object[lop_or_name];
+        return make_logical_operator_predicate(lop_or_name, value);
+      } else {
+        const sub_field = checked_condition_object[lop_or_name];
+        const keys2 = Object.keys(sub_field);
+        if (keys2.length === 1) {
+          const cop = keys2[0];
+          const value = sub_field[cop];
+          return make_comparison_operator_predicate(cop, lop_or_name, value);
+        }
+      }
+    }
+    // TODO: Error message
+    throw new TypeError();
+  }
+
+  function make_logical_operator_predicate(logical_operator, operands) {
+    const checked_logical_operator = Utils.checkNonEmptyStringSW(logical_operator, '$');
+    const checked_operands = Utils.checkArray(operands);
+    const builder = get_lop_builder(checked_logical_operator);
+    for (const operand of checked_operands) {
+      const predicate = make_predicate(operand);
+      builder.add(predicate);
+    }
+    return builder.build();
+  }
+
+  function get_lop_builder(logical_operator) {
+    const predicates = [];
+
+    function add(predicate) {
+      predicates.push(predicate);
+    };
+
+    function build_and() {
+      return function(target) {
+        for (const predicate of predicates) {
+          if (!predicate(target)) {
+            return false;
+          }
+        }
+        return true;
+      };
+    }
+
+    function build_or() {
+      return function(target) {
+        for (const predicate of predicates) {
+          if (predicate(target)) {
+            return true;
+          }
+        }
+        return false;
+      };
+    }
+
+    switch (logical_operator) {
+      case '$and':
+        return {
+          'add': add,
+          'build': build_and
+        };
+      case '$or':
+        return {
+          'add': add,
+          'build': build_or
+        };
+      default:
+        // TODO: Error message
+        throw new TypeError();
+    }
+    return builder;
+  }
+
+  function make_comparison_operator_predicate(operator, name, value) {
+    switch(operator) {
+      case '$eq':
+        return function(target) {
+          return (target[name] === value);
+        };
+      case '$gt':
+        return function(target) {
+          return (target[name] > value);
+        };
+      case '$gte':
+        return function(target) {
+          return (target[name] >= value);
+        };
+      case '$lt':
+        return function(target) {
+          return (target[name] < value);
+        };
+      case '$lte':
+        return function(target) {
+          return (target[name] <= value);
+        };
+      case '$ne':
+        return function(target) {
+          return (target[name] !== value);
+        };
+      default:
+        // TODO: Error message
+        throw new TypeError();
+    }
+  }
+
+  function make_consumer(action_array) {
+    const consumers = [];
+    const checked_action_array = Utils.checkArray(action_array);
+    for (const action_object of checked_action_array) {
+      const keys = Object.keys(action_object);
+      if (keys.length === 1) {
+        const uop = keys[0];
+        const sub_field = action_object[uop];
+        const keys2 = Object.keys(sub_field);
+        if (keys2.length === 1) {
+          const name = keys2[0];
+          const value = sub_field[name];
+          const consumer = make_update_operator_consumer(uop, name, value);
+          consumers.push(consumer);
+          continue;
+        }
+      }
+      // TODO: Error message
+      throw new TypeError();
+    }
+    return function(target) {
+      for (const consumer of consumers) {
+        consumer(target);
+      }
+    };
+  }
+
+  function make_update_operator_consumer(operator, name, value) {
+    switch (operator) {
+      case '$inc':
+        return function(target) {
+          target[name] += value;
+        };
+      case '$set':
+        return function(target) {
+          target[name] = value;
+        };
+      case '$addToSet':
+        return function(target) {
+          if (target.indexOf(value) === -1) {
+            target.push(value);
+          }
+        };
+      case '$push':
+        return function(target) {
+          target.push(value);
+        }
+      default:
+        // TODO: Error message
+        throw new TypeError();
+    }
+  }
+
+  const predicate = make_predicate(canonicalized_trigger_object.condition);
+  const consumer = make_consumer(canonicalized_trigger_object.action);
+  return function(target) {
+    if (predicate(target)) {
+      consumer(target);
+      return true;
+    } else {
+      return false;
+    }
   };
 }
 
@@ -196,18 +373,18 @@ class TriggerCompiler {
     const condition_object = trigger_object.condition;
     const action_object = trigger_object.action;
     const canonicalized_condition_object = canonicalize_condition(trigger_object.condition);
-    const optimized_condition_object = optimized_condition(canonicalized_condition_object);
-    const canonicalized_action_object = canonicalize_action(trigger_object.action);
-    const optimized_trigger_object = {
+    const optimized_condition_object = optimize_condition(canonicalized_condition_object);
+    const canonicalized_action_array = canonicalize_action(trigger_object.action);
+    const canonicalized_trigger_object = {
       'name': name,
       'queue': queue,
       'target': target,
       'condition': optimized_condition_object,
-      'action': canonicalized_action_object
+      'action': canonicalized_action_array
     };
-    const trigger_function = compile_trigger(optimized_trigger_object);
+    const trigger_function = compile_trigger(canonicalized_trigger_object);
     return {
-      source: optimized_trigger_object,
+      source: canonicalized_trigger_object,
       function: trigger_function
     };
   }
